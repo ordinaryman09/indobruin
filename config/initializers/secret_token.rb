3914c82c972843bf97e842ccc155a273d388a930
Richard <weilung.nscc@gmail.com>
2011-09-30 00:21:43 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibsa::Application.config.secret_token = 'c5a0a70f07e368003a207f0cd6c592469cb3165262e771b91116eb03569b14548fd11026f40456cee15134ec0d6b496ed7d7d4a2d39afaaee7966a25c2254195'
